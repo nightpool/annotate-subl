48b1d04df0a9135c4d2d681418336c4024d5ebbb
evan <eg1290@gmail.com>
2015-05-22 16:19:34 -0500

    Heroku stuffs!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec ruby oauth.rb -p $PORT
--- oauth.rb
+++ oauth.rb
@@ -1,28 +1,44 @@
-#!/usr/local/ruby/bin/ruby
-
-client_secret = File.open('client-secret').read.strip
 
 require 'sinatra'
 require 'rest-client'
-
-client_id = "ciUq7-YBs7OvIBqyEbX6CTMFwBzxXfC_0bMmMPsXfLWopNHyVm3ZfCTpyDtuSj6n"
-redirect_uri = "http://nightpool.me/annotate-subl-auth"
-api_path = "https://api.genius.com"
 
 helpers do 
     def payload(code)
         {
           "code": code,
-          "client_id": client_id,
-          "client_secret": client_secret,
-          "redirect_uri": redirect_uri,
+          "client_id": ENV['client_id'],
+          "client_secret": ENV['client_secret'],
+          "redirect_uri": ENV['redirect_uri'],
           "response_type": "code",
           "grant_type": "authorization_code"
         }
     end
 
     def get_token(code)
-        RestClient.post "#{api_path}/oauth/token", payload(code), :accept => :json
+        RestClient.post "#{ENV['api_path']}/oauth/token", payload(code), :accept => :json
+    end
+
+    def install
+        <<-EOF
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Annotate Sublime Installation</title>
+                <style type="text/css">
+                    
+                </style>
+            </head>
+            <body>
+
+            <h1>Authorization Successful</h1>
+
+            <p>Copy the following code and paste it into Sublime:</p>
+
+            <textarea readonly="true" onclick="this.select();this.setSelectionRange(0, 9999);"><%= token %></textarea>
+
+            </body>
+            </html>
+        EOF
     end
 end
 
@@ -31,25 +47,4 @@ get '/auth' do
     erb install, locals: {token: get_token(code)}
 end
 
-install = <<EOF
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Annotate Sublime Installation</title>
-    <style type="text/css">
-        
-    </style>
-</head>
-<body>
-
-<h1>Authorization Successful</h1>
-
-<p>Copy the following code and paste it into Sublime:</p>
-
-<textarea readonly="true" onclick="this.select();this.setSelectionRange(0, 9999);"><%= token %></textarea>
-
-</body>
-</html>
-EOF
-
-# set :server, "CGI"
+set :server, "webrick"
